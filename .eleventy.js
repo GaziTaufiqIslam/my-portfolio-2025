477222cabcd637d0ac4f26bdeaca9a82c607f2de
Big G <Big_G@Mac-Studio.local>
2025-11-15 19:51:56 +0530

    updated home page design

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
+// This is the simple, correct Eleventy config.
+// Its only job is to build HTML and copy the Webpack-built assets.
+
 module.exports = function(eleventyConfig) {
   
-  // This is the correct copy command
+  // 1. Copy the ENTIRE 'src/assets' folder (which Webpack built)
   eleventyConfig.addPassthroughCopy("./src/assets/");
-
-  // We still copy lottie files
+  
+  // 2. Copy your lottie files
   eleventyConfig.addPassthroughCopy("src/lottie");
 
   return {
